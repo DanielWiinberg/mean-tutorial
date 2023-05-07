97e09e21e0885e9ed19bd8be478464261260ea94
danieldanwii <daniel.wiinberg@norconsult.com>
2023-05-07 18:15:10 +0200

    Added login and signup links and forms to angular frontend

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-post-list',
@@ -13,6 +14,9 @@ export class PostListComponent implements OnInit, OnDestroy{
   posts: Post[] = [];
   postUpdateSubscription: Subscription;
   isLoading: boolean = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
 
   constructor(private postsService: PostsService){
   }
@@ -33,5 +37,10 @@ export class PostListComponent implements OnInit, OnDestroy{
 
   onDeletePost(postId: string){
     this.postsService.deletePost(postId);
+  }
+
+  onChangedPage(pageEvent: PageEvent){
+    console.log(pageEvent);
+    
   }
 }
